d068d4c62ebe469c671aefeae3eff6a3b7816c22
siddharth <siddharth.ashutosh7@gmail.com>
2023-03-11 13:34:20 +0530

    new game initialized

---
+++ core/application.js
@@ -0,0 +1,41 @@
+import { Application, Assets, Sprite } from 'pixi.js';
+import bunny1 from '../assets/bunny.png'
+
+const app = new Application( {width: 1280, height: 720, backgroundColor: 0x000000, autoResize: true, resolution: window.devicePixelRatio > 1 ? 2:1 });
+globalThis.__PIXI_APP__ = app;
+
+document.getElementById('canvasContainer').appendChild(app.view);
+
+const texture = await Assets.load(bunny1);
+const bunny = new Sprite(texture);
+// Setup the position of the bunny
+bunny.x = app.renderer.width / 2;
+bunny.y = app.renderer.height / 2;
+
+// Rotate around the center
+bunny.anchor.x = 0.5;
+bunny.anchor.y = 0.5;
+
+// Add the bunny to the scene we are building
+app.stage.addChild(bunny);
+
+// Listen for frame updates
+app.ticker.add(() => {
+    // each frame we spin the bunny around a bit
+    bunny.rotation += 0.01;
+});
+var resizeCanvasContainer = function () {
+    /* our scaling is based on 1280x720*/
+    var maxWidth = 1280;
+    var maxHeight = 720;
+    var canvasMargin = 0.9;
+    var ratio = Math.min(window.innerWidth / maxWidth, window.innerHeight / maxHeight) * canvasMargin;
+
+    if (window.innerWidth * canvasMargin < maxWidth || window.innerHeight * canvasMargin < maxHeight) {
+        document.getElementById("canvasContainer").style.transform = "scale(" + ratio + ")";
+    }
+};
+resizeCanvasContainer();
+window.onresize = function(){
+    resizeCanvasContainer();
+}
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  //This property defines where the application starts
+  entry:'./core/application.js',
+    
+  //This property defines the file path and the file name which will be used for deploying the bundled file
+  output:{
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+    
+  //Setup loaders
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      
+    ]
+  },
+  experiments: {
+    topLevelAwait: true
+  },
+    
+  // Setup plugin to use a HTML file for serving bundled js files
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+}
