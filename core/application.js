b9adf21eea64763a42fa275936d5640787c4a7f5
siddharth <siddharth.ashutosh7@gmail.com>
2023-03-12 12:06:03 +0530

    mobile changes

--- core/application.js
+++ core/application.js
@@ -1,7 +1,13 @@
 import { Application, Assets, Sprite } from 'pixi.js';
+import {isMobile} from 'react-device-detect';
 import bunny1 from '../assets/bunny.png'
 
-const app = new Application( {width: 1280, height: 720, backgroundColor: 0x000000, autoResize: true, resolution: window.devicePixelRatio > 1 ? 2:1 });
+let app;
+if(isMobile){
+    app = new Application( {width: 375, height: 667, backgroundColor: 0x000000, autoResize: true, resizeTo: window, resolution: window.devicePixelRatio > 1 ? 1:1 });
+}else{
+    app = new Application( {width: 1280, height: 720, backgroundColor: 0x000000, autoResize: true, resolution: window.devicePixelRatio > 1 ? 2:1 });
+}
 globalThis.__PIXI_APP__ = app;
 
 document.getElementById('canvasContainer').appendChild(app.view);
@@ -22,9 +28,13 @@ app.stage.addChild(bunny);
 // Listen for frame updates
 app.ticker.add(() => {
     // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
+    //bunny.rotation += 0.01;
 });
+
 var resizeCanvasContainer = function () {
+    if(isMobile){
+        return
+    }
     /* our scaling is based on 1280x720*/
     var maxWidth = 1280;
     var maxHeight = 720;
@@ -35,8 +45,17 @@ var resizeCanvasContainer = function () {
         document.getElementById("canvasContainer").style.transform = "scale(" + ratio + ")";
     }
 };
-resizeCanvasContainer();
 window.onresize = function(){
-    resizeCanvasContainer();
+    if(isMobile){
+        bunny.x = app.renderer.width / 2;
+        bunny.y = app.renderer.height / 2;
+        let scale  = window.innerWidth/375;
+        bunny.scale.set(scale*0.5);
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+    }else{
+        resizeCanvasContainer();
+    }
+
 }
+window.onresize();
 
